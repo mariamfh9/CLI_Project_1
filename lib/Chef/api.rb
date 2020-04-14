da29d9e27a7ed39d8f052734d83b775e7624d2f4
mariamfh9 <mfh.haider@gmail.com>
2020-04-14 13:18:08 -0400

    new_ingredient in API

--- lib/Chef/api.rb
+++ lib/Chef/api.rb
@@ -2,10 +2,14 @@ class Api
 
     def self.cook(ingredient)
         url = "https://www.themealdb.com/api/json/v1/1/filter.php?i=#{ingredient}"
-        response = Net::HTTP.get(URI(url))
-        recipes = JSON.parse(response)["meals"]
-        recipes.each {|d| Recipe.new(name: d["strMeal"], meal_id: d["idMeal"], ingredient: ingredient)}
-
+        response = Net::HTTP.get(URI(url))  #to get back JSON in strings
+        recipes = JSON.parse(response)["meals"] #usable JSON, recipes points at an array of meals
+        new_ingredient = Ingredient.new(ingredient) #creates a new ingredient
+        recipes.each do |d| #go through all the recipes and make a new meal for each one
+           new_recipe = Recipe.new(name: d["strMeal"], meal_id: d["idMeal"], ingredient: ingredient)
+           new_ingredient.recipes << new_recipe #relationship between ingredient and recipe
+        end 
+        binding.pry
     end 
 
     def self.get_recipe_details(recipe)
